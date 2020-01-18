aeb3abe6bdd0a7feb4f7af45c1ed562de7322a50
Meatball <mainmeatball1@gmail.com>
2020-01-19 00:41:39 +0300

    create custom Pair<L,R> class;

---
+++ src/sample/Pair.java
@@ -0,0 +1,20 @@
+package sample;
+
+public class Pair<L, R> {
+
+    private L start;
+    private R end;
+
+    public Pair(L start, R end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public L getStart() {
+        return start;
+    }
+
+    public R getEnd() {
+        return end;
+    }
+}
